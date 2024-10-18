5be87f7d42e89b475ee1a4922cda7374f128cbd4
NguyenHuyen0311 <huyenmeroria@gmail.com>
2024-10-18 21:00:21 +0700

    Học ReactJS Tiết 6

---
+++ ReactJS/B6/learn-react/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+// import Todos from './components/Todos';
+// import ProductReducer from './components/ProductReducer';
+import ProductState from './components/ProductState';
+// import CounterReducer from './components/CounterReducer';
+// import CounterState from './components/CounterState';
+// import Counter from './components/Counter';
+
+function App() {
+  return (
+    <>
+      {/* <Counter /> */}
+      {/* <CounterState /> */}
+      {/* <CounterReducer /> */}
+      <ProductState />
+      {/* <ProductReducer /> */}
+      {/* <Todos /> */}
+    </>
+  );
+}
+
+export default App;
